3a5b0e5242162c00b1370a8aeed13a3f51a7860b
Francis Gulotta <wizard@roborooter.com>
2021-01-03 21:44:59 -0500

    try to get tokio up and going

--- src/lib.rs
+++ src/lib.rs
@@ -4,46 +4,28 @@
 extern crate napi_derive;
 
 use std::convert::TryInto;
-
 use napi::{CallContext, Env, JsNumber, JsObject, Result, Task};
-
-struct AsyncTask(u32);
-
-impl Task for AsyncTask {
-  type Output = u32;
-  type JsValue = JsNumber;
-
-  fn compute(&mut self) -> Result<Self::Output> {
-    use std::thread::sleep;
-    use std::time::Duration;
-    sleep(Duration::from_millis(self.0 as u64));
-    Ok(self.0 * 2)
-  }
-
-  fn resolve(self, env: Env, output: Self::Output) -> Result<Self::JsValue> {
-    env.create_uint32(output)
-  }
-}
+use serialport::{available_ports};
+use tokio::{self,task};
 
 #[module_exports]
 fn init(mut exports: JsObject) -> Result<()> {
-  exports.create_named_method("sync", sync_fn)?;
-
-  exports.create_named_method("sleep", sleep)?;
+  exports.create_named_method("list", list)?;
+  println!("module loaded");
   Ok(())
 }
 
 #[js_function(1)]
-fn sync_fn(ctx: CallContext) -> Result<JsNumber> {
-  let argument: u32 = ctx.get::<JsNumber>(0)?.try_into()?;
+pub fn list(ctx: CallContext) -> Result<JsObject> {
 
-  ctx.env.create_uint32(argument + 100)
+  let task = task::spawn_blocking(|| {
+    available_ports()
+  });
+
+  ctx.env.execute_tokio_future(
+    task,
+    |&mut env, data| env.create_buffer_with_data(data).map(|v| v.into_raw()),
+  )
 }
 
-#[js_function(1)]
-fn sleep(ctx: CallContext) -> Result<JsObject> {
-  let argument: u32 = ctx.get::<JsNumber>(0)?.try_into()?;
-  let task = AsyncTask(argument);
-  let async_task = ctx.env.spawn(task)?;
-  Ok(async_task.promise_object())
-}
+
